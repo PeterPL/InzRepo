e7be09cfccc8d0d8622e7a853244e418bb668d8d
PeterPL <piotrek.opielka@gmail.com>
2014-09-09 11:29:09 +0200

    DSP.cs added and functions for microphone

--- InzApp/MainPage.xaml.cs
+++ InzApp/MainPage.xaml.cs
@@ -8,19 +8,54 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using InzApp.Resources;
+using System.Windows.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using System.IO;
+
 
 namespace InzApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
+        Microphone microphone = Microphone.Default;
+        byte[] buffer;
+        MemoryStream stream = new MemoryStream();
+        SoundEffect sEffect;
         // Constructor
         public MainPage()
         {
             InitializeComponent();
 
+            DispatcherTimer dispTimer = new DispatcherTimer();
+            dispTimer.Interval = TimeSpan.FromMilliseconds(50);
+            dispTimer.Tick += delegate { try { FrameworkDispatcher.Update(); } catch { } };
+            dispTimer.Start();
+
+            microphone.BufferReady += new EventHandler<EventArgs>(microphone_BufferReady);
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
+
+        private void microphone_BufferReady(object sender, EventArgs e)
+        {
+            microphone.GetData(buffer);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        private void startRecording(object sender, RoutedEventArgs e)
+        {
+            microphone.BufferDuration = TimeSpan.FromMilliseconds(100);
+            buffer = new byte[microphone.GetSampleSizeInBytes(microphone.BufferDuration)];
+            microphone.Start();
+        }
+
+        private void stopRecording(object sender, RoutedEventArgs e)
+        {
+            microphone.Stop();
+        }
+
 
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
