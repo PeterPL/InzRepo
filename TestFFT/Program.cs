0a1029eec03da34a99c9e5c62858733a2ac64c17
PeterPL <piotrek.opielka@gmail.com>
2014-09-11 15:03:36 +0200

    ...

--- TestFFT/Program.cs
+++ TestFFT/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using NAudio.Wave;
+using NAudio;
+using System.Collections;
+using DSP;
 
 namespace TestFFT
 {
@@ -10,7 +15,25 @@ namespace TestFFT
     {
         static void Main(string[] args)
         {
-            
+            /*WaveFileReader wavReader = new WaveFileReader(@"D:\Priv\ProjektInz\InzApp\TestFFT\SoundSamples\vocal2.wav");
+            ArrayList wavArray = new ArrayList();
+            for (int i = 0; i < wavReader.Length; i++){
+                     
+               int newArrayElement =   wavReader.
+                wavArray.Add(newArrayElement);
+            }
+             */
+            byte[] byteArray = File.ReadAllBytes(@"D:\Priv\ProjektInz\InzApp\TestFFT\SoundSamples\vocal2.wav");
+            StreamWriter writer = new StreamWriter(@"wav.txt");
+            foreach (int x in byteArray)
+            {
+                writer.Write(x + " ");
+            }
+
+            double[] sampleBuffer = new Double[
+           
+            Console.ReadKey();
+
         }
     }
 }
